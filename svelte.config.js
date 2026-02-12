033c3ac3838b8f6fcb49acf1bdb7ede606fa250d
Daniel <danielhart1196@gmail.com>
2026-02-12 19:40:16 +1100

    creation

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
-let adapterMod;
-try {
-  adapterMod = await import('@sveltejs/adapter-auto');
-} catch {}
+import adapter from '@sveltejs/adapter-static';
+
+const isGhPages = process.env.GH_PAGES === 'true';
 
 export default {
   kit: {
-    adapter: adapterMod ? adapterMod.default() : undefined,
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: '404.html'
+    }),
+    paths: {
+      base: isGhPages ? '/arr-nba' : ''
+    },
     alias: {
       $lib: 'src/lib'
+    },
+    prerender: {
+      entries: ['*']
     }
   }
 };
